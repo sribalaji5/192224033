c5ba414b9658b8780ae4234584bd1eea41fa9631
sribalaji5 <122214695+sribalaji5@users.noreply.github.com>
2023-01-09 11:47:58 +0530

    Add files via upload

---
+++ pro5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	int b,c,d,i,j,n;
+	char a[]={'s','a','v','e','e','t','h','a'};
+	n=sizeof(a)/sizeof(a[0]);
+	printf("%d",n);
+}
+
